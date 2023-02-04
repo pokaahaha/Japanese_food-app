4be912af7122f71ed320d4018d3aae1bcb1ea26d
pokaahaha <men101@protonmail.com>
2023-02-04 16:57:27 +0300

    add items

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './MealItem.module.css'
+export const MealItem = (props) => {
+  const formattedPrice = `$${props.price.toFixed(2)}`
+  return (
+    <li className={styles.meal}>
+      <div><h3>{props.name}</h3>
+      <div className={styles.description}>{props.description}</div>
+      <div className={styles.price}>{formattedPrice}</div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+  );
+};
