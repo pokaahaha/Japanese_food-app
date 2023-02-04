76e7b97b39497496e9be43978cb44ea17f73aceb
pokaahaha <men101@protonmail.com>
2023-02-04 19:31:47 +0300

    add refs

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,30 @@
+import { useRef, useState } from "react";
 import { Input } from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 
 export const MealItemForm = (props) => {
+  const [isAmountValid, setIsAmountValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const inputAnount = amountInputRef.current.value;
+    if (
+      inputAnount.trim().length === 0 ||
+      +inputAnount < 1 ||
+      +inputAnount > 10
+    ) {
+      setIsAmountValid(false)
+      return;
+    }
+
+    props.onAddToCart(+inputAnount)
+  };
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Количество"
         input={{
           id: props.id,
@@ -15,6 +35,7 @@ export const MealItemForm = (props) => {
         }}
       />
       <button>Добавить</button>
+      {!isAmountValid && <p>Введите Количество от 1 до 10</p>}
     </form>
   );
 };
